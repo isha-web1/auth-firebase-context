54ca1eac1d2edd8173632d55aa9a5957a129f495
isha-web1 <isha.web1@gmail.com>
2023-04-24 13:57:22 +0600

    login page added

--- src/component/LogIn/LogIn.jsx
+++ src/component/LogIn/LogIn.jsx
@@ -1,11 +1,62 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
-    return (
-        <div>
-            <h1>this is login page</h1>
+    const handleLogin= event =>{
+        event.preventDefault()
+       const form = event.target;
+       const email = form.email.value
+       const password = form.password.value
+       console.log(email, password)
+    }
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col ">
+        <div className="text-center ">
+          <h1 className="text-5xl font-bold">Please Login !!</h1>
         </div>
-    );
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleLogin} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+          <Link to='/register'>
+          <button className="btn btn-link">New to auth-firebase-context?</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
